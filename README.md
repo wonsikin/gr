115762cf36dc36998cbc225d981dc12cefe19e1a
Arthur Wang <fnghwsj@gmail.com>
2018-04-02 17:22:58 +0800

    another commit

---
+++ README.md
@@ -0,0 +1,2 @@
+goreleaser-example
+------------------
--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import "fmt"
+
+var version = "master"
+
 func main() {
+	fmt.Printf("version is %s\n", version)
 	println("Ba dum, tss!")
 }
